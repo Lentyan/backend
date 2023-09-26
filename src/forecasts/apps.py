284f4a7a5aa14b32c30f683b7dd4c553a891af64
Михаил Касаткин <m.m.kasatkin@yandex.ru>
2023-09-26 22:52:35 +0300

    feature: forecasts application created.

---
+++ src/forecasts/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class ForecastsConfig(AppConfig):
+    """Forecasts app configs."""
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "forecasts"
