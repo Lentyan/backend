bc1e26585a1e59b178eb5a7c1cb72613be75b575
Михаил Касаткин <m.m.kasatkin@yandex.ru>
2023-09-29 12:10:15 +0300

    feature: documentation endpoints added.

--- src/api/v1/urls.py
+++ src/api/v1/urls.py
@@ -1,9 +1,35 @@
+from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 from rest_framework.routers import DefaultRouter
 
 from api.v1 import views
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API",
+        default_version="v1",
+        description="Description",
+    ),
+    public=True,
+)
 
 v1_router = DefaultRouter()
 v1_router.register(r"categories", views.SKUViewSet, basename="categories")
 v1_router.register(r"shops", views.StoreViewSet, basename="shops")
 v1_router.register(r"sales", views.SaleViewSet, basename="sales")
 v1_router.register(r"forecasts", views.ForecastViewSet, basename="forecasts")
+
+urlpatterns = [
+    path("", include(v1_router.urls)),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path(
+        "redoc/",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
+]
